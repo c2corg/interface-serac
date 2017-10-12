76925e84430e0bf8a8a5aa63e1b73ab20fc4a935
Marc Fournier <marc.fournier@camptocamp.com>
2017-10-12 21:15:54 +0200

    switch DSN to pgsql

--- partials/db.php
+++ partials/db.php
@@ -3,9 +3,8 @@ $host = '127.0.0.1';
 $db   = 'interface-serac';
 $user = 'user';
 $pass = 'password';
-$charset = 'utf8';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "pgsql:host=$host;dbname=$db";
 $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
