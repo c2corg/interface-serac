bd99d29d2f31647108cb59eb35747df10db64dc1
Marc Fournier <marc.fournier@camptocamp.com>
2017-11-26 08:48:31 +0100

    add postgres variant of database schema

---
+++ install/import-pgsql.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE reports (
+  id SERIAL,
+  id_provider int NOT NULL,
+  id_user int NOT NULL,
+  pushed smallint NOT NULL,
+  visibility smallint NOT NULL,
+  json_serac text NOT NULL,
+  json_custom text NOT NULL,
+  title varchar(250) NOT NULL,
+  activities text NOT NULL,
+  event_type varchar(250) NOT NULL,
+  date_start date NOT NULL,
+  date_end date NOT NULL,
+  elevation smallint NOT NULL,
+  severity varchar(30) NOT NULL,
+  nb_participants smallint NOT NULL,
+  nb_impacted smallint NOT NULL,
+  quality varchar(30) NOT NULL,
+  language varchar(2) NOT NULL,
+  created_at timestamp NOT NULL,
+  updated_at timestamp NOT NULL
+);
+
+ALTER TABLE reports
+  ADD PRIMARY KEY (id);
